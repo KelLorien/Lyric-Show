bd6a6d9102d8e295c6e29733e9a422ccf20561b2
KelLorien <jpipe@systemsinmotion.com>
2012-10-10 17:20:54 -0400

    Added a test class.

---
+++ src/nameless/HelloWorld.java
@@ -0,0 +1,15 @@
+package nameless;
+
+/**
+ * User: jpipe
+ * Date: 10/10/12
+ * Time: 5:18 PM
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("This class will likely be deleted in final release, or sooner!");
+    }
+
+}
