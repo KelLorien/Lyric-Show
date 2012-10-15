ac518b391abedcd1ff3c42cadfcd74d8d6374be7
KelLorien <jpipe@systemsinmotion.com>
2012-10-15 19:16:27 -0400

    HelloWorld now produces a powerpoint file saying Hello World.

--- src/nameless/HelloWorld.java
+++ src/nameless/HelloWorld.java
@@ -1,5 +1,12 @@
 package nameless;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.hslf.model.Slide;
+import org.apache.poi.hslf.usermodel.SlideShow;
+
 /**
  * User: jpipe
  * Date: 10/10/12
@@ -8,8 +15,35 @@ package nameless;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("This class will likely be deleted in final release, or sooner!");
+        createPowerPoint();
+    }
+
+    public static void createPowerPoint() {
+        SlideShow show = new SlideShow();
+
+        Slide s1 = show.createSlide();
+
+        s1.addTitle().setText("Hello There");
+
+        FileOutputStream outputStream;
+        try {
+            outputStream = new FileOutputStream("pwr.ppt");
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not create file!");
+            return;
+        }
+
+        try {
+            show.write(outputStream);
+        } catch (IOException e) {
+            System.out.println("Could not write the file!");
+        } finally {
+            try {
+                outputStream.close();
+            } catch (IOException e) {
+                //ignore
+            }
+        }
     }
 
 }
