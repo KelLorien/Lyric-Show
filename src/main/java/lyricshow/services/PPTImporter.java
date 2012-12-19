2f57ba4743f5f89482ddcd6a3b52dc55b2bc34b0
KelLorien <jpipe@systemsinmotion.com>
2012-12-19 16:08:03 -0500

    fixed skipping first slide bug

--- src/main/java/lyricshow/services/PPTImporter.java
+++ src/main/java/lyricshow/services/PPTImporter.java
@@ -31,6 +31,12 @@ public class PPTImporter {
     private PPTImporter() {
     }
 
+    /**
+     * Imports the songs stored in a PowerPoint file.
+     * @param ppt {@link File} representing the powerpoint file to be read.
+     * @return A list of {@link Song} objects, representing the data stored in the given powerpoint file.
+     * @throws IOException
+     */
     public List<Song> importSong(File ppt) throws IOException {
         List<Song> songs = new ArrayList<Song>();
 
@@ -41,8 +47,13 @@ public class PPTImporter {
         return songs;
     }
 
-
-
+    /**
+     * Reads in the powerpoint.
+     * @param ppt {@link File} pointing to the .ppt file to be read.
+     * @return an instantiated {@link SlideShow} representing the given file. The file is not changed in
+     * any way by this method
+     * @throws IOException If there was an exception reading the file.
+     */
     private SlideShow readSlideShow(File ppt) throws IOException {
         FileInputStream inputStream = null;
         SlideShow show = null;
@@ -62,12 +73,21 @@ public class PPTImporter {
         return show;
     }
 
+    /**
+     * Parses all the songs in the show and populates the given list with them.
+     * Determines the end/beginning of songs bases on
+     * {@link org.apache.poi.hslf.model.Slide#getTitle()}. Whenever the title changes, a new
+     * slide is added.
+     *
+     * @param songs The list to be populated with songs.
+     * @param show The {@link SlideShow} to be parsed.
+     */
     private void parseAllSongs(List<Song> songs, SlideShow show) {
         Slide[] slides = show.getSlides();
         if (slides.length < 0)
             return;
 
-        Song currentSong = startNewSong(slides[0]);
+        Song currentSong = startNewSong(slides[0], true);
         for (Slide slide: slides) {
             String nextTitle = removeNewLines(slide.getTitle());
             if (nextTitle == null || slide.getTextRuns().length < 2) {
@@ -78,19 +98,31 @@ public class PPTImporter {
                         + slide.getTextRuns()[1].getText().trim() + "\n\n");
             } else {
                 songs.add(currentSong.copy());
-                currentSong = startNewSong(slide);
+                currentSong = startNewSong(slide, false);
             }
         }
         songs.add(currentSong);
     }
 
-    private Song startNewSong(Slide slide) {
+    /**
+     * Creates a new song based on the information given by the {@link Slide} object passed in.
+     * @param slide containing the relevant information for this song.
+     * @param addLyrics designates whether the lyrics from this slide should be added to the returned
+     *                  song. True means the lyrics contained in this slide will be added to the returned song
+     *                  (in the second {@link TextRun}, according to
+     *                  {@link org.apache.poi.hslf.model.Slide#getTextRuns()})
+     * @return A song instantiated with all the information given on the page.
+     */
+    private Song startNewSong(Slide slide, boolean addLyrics) {
         Song song = new Song();
 
         song.setTitle(removeNewLines(slide.getTitle()));
-        song.setLyrics("");
 
         TextRun[] textRuns = slide.getTextRuns();
+
+        if (addLyrics)
+            song.setLyrics(textRuns[1].getText().trim() + "\n\n");
+
         if (textRuns.length >= 3) {
             song.setAuthor(removeNewLines(textRuns[2].getText()));
         } else {
